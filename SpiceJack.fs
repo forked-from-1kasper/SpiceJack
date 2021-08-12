588011352f4251bc2b97e23d690d570fd26d3bfd
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-08-12 16:52:49 +0700

    limit memory usage

--- SpiceJack.fs
+++ SpiceJack.fs
@@ -1,5 +1,5 @@
 open System
-open System.Collections.Concurrent
+open System.Collections.Generic
 
 open SpiceSharp
 open SpiceSharp.Components
@@ -10,35 +10,41 @@ open JackSharp
 open JackSharp.Ports
 open JackSharp.Processing
 
+let bufferCapacity = 1024 * 8
+let jackDelay = 1000
 let freq = 44100.0
 
 let waveform = Pulse (0.0, 0.5, 1.0 / 5000.0, 0.0, 0.0, 1.0 / 5000.0, 2.0 / 5000.0)
-let circuit = Circuit (VoltageSource ("V1", "0", "in", waveform), Capacitor ("C1", "in", "out", 1.0), Resistor ("R1", "out", "0", 2.0e4))
+
+let circuit = Circuit (VoltageSource ("V1", "0", "A", waveform), Resistor ("R1", "A", "B", 10.0), Resistor ("R2", "B", "C", 100.0), Capacitor ("C1", "C", "0", 1.0))
 
 type Quantity =
     | Voltage of string
     | Current of string
 
-let ports = [Voltage "0"; Voltage "out"]
+let ports = [Voltage "A"; Voltage "C"]
 
 type RealExport = IExport<double>
 type ExportQueue(sim, quantity) =
-    let queue = new ConcurrentQueue<float32> ()
+    let queue = new Queue<float32> ()
     let export : RealExport =
         match quantity with
         | Voltage port -> new RealVoltageExport (sim, port) :> RealExport
         | Current device -> new RealCurrentExport (sim, device) :> RealExport
 
+    member this.Monitor = new Object ()
     member val Measured = 0 with get, set
 
     member this.Write () =
-        queue.Enqueue (float32 export.Value)
-        this.Measured <- this.Measured + 1
+        while this.Measured >= bufferCapacity do ()
+
+        lock this.Monitor (fun () ->
+            queue.Enqueue (float32 export.Value)
+            this.Measured <- this.Measured + 1)
 
     member this.Receive () =
-        let b, v = queue.TryDequeue ()
         this.Measured <- this.Measured - 1
-        v
+        queue.Dequeue ()
 
 let processFunc (queues : ExportQueue list) (chunk : ProcessBuffer) =
     let buffCount = chunk.AudioOut.Length
@@ -47,13 +53,14 @@ let processFunc (queues : ExportQueue list) (chunk : ProcessBuffer) =
     List.iteri (fun i (queue : ExportQueue) ->
         while queue.Measured < buffSize do ()
 
-        for j in 0 .. buffSize - 1 do
-            chunk.AudioOut.[i].Audio.[j] <-
-                queue.Receive ()) queues
+        lock queue.Monitor (fun () ->
+            for j in 0 .. buffSize - 1 do
+                chunk.AudioOut.[i].Audio.[j] <-
+                    queue.Receive ())) queues
 
 let runJack (client : Processor) =
     async {
-        do! Async.Sleep 1000
+        do! Async.Sleep jackDelay
         ignore (client.Start ())
         printfn "Jack client started"
     }
